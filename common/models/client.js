458cfd69cc1a9e1a991da747426d3d581c6dffd8
Dan Boehmke <Danboe2009@gmail.com>
2021-12-22 16:08:06 -0500

    Create client.js

---
+++ common/models/client.js
@@ -0,0 +1,13 @@
+var properties = {
+    fName: {
+      type: String,
+      required: true
+    },
+    lName: {
+      type: String,
+      required: true
+    }
+  };
+  
+  var client = loopback.Model.extend('user', properties);
+  
